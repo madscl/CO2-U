c83876e77462068aa91a257cd73ab3cc4005952a
Madeleine Clore <madeleine.clore@gmail.com>
2019-11-20 09:21:15 -0800

    createCalculationScreen - created new screen and got rid of settings screen

--- CO2-U/navigation/MainTabNavigator.js
+++ CO2-U/navigation/MainTabNavigator.js
@@ -5,7 +5,7 @@ import { createStackNavigator, createBottomTabNavigator } from 'react-navigation
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
 import LinksScreen from '../screens/LinksScreen';
-import AchievementsScreen from '../screens/SettingsScreen';
+import AchievementsScreen from '../screens/AchievementsScreen';
 
 const config = Platform.select({
   web: { headerMode: 'screen' },
