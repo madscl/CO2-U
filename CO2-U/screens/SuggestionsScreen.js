92029a8538e532ac3a6175016278b0418fe54da7
echurley5 <erica.c.hurley@gmail.com>
2019-11-22 11:10:41 -0800

    SuggestionsText-new format of suggestions screen

--- CO2-U/screens/SuggestionsScreen.js
+++ CO2-U/screens/SuggestionsScreen.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+//import { ScrollView, StyleSheet } from 'react-native';
+//import { ExpoLinksView } from '@expo/samples';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
 export default function SuggestionsScreen() {
   return (
     <ScrollView style={styles.container}>
-      {/**
-       * Go ahead and delete ExpoLinksView and replace it with your content;
-       * we just wanted to provide you with some helpful links.
-       */}
-      <ExpoLinksView />
+      <Text style={styles.getStartedText}>Here's how to reduce your carbon footprint</Text>
+      <Text style={styles.getStartedText}></Text>
+      <Text style={styles.getAttentionText}>Daily Challenge:</Text> 
+      <Text style={styles.dailyChallengeText}>Try to eat 2 plant based meals today!</Text>
     </ScrollView>
   );
 }
@@ -23,5 +31,27 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 15,
     backgroundColor: '#fff',
+  },
+  getStartedContainer: {
+    alignItems: 'center',
+    marginHorizontal: 50,
+  },
+  getStartedText: {
+    fontSize: 22,
+    color: 'rgba(0 ,100 ,0, 1)',
+    lineHeight: 24,
+    textAlign: 'center',
+  },
+  getAttentionText: {
+    fontSize: 20,
+    color: 'rgba(0 ,0 ,0, 100)',
+    lineHeight: 24,
+    textAlign: 'center',
+  },
+  dailyChallengeText: {
+    fontSize: 16,
+    color: 'rgba(0 ,0 ,0, 100)',
+    lineHeight: 24,
+    textAlign: 'center',
   },
 });
