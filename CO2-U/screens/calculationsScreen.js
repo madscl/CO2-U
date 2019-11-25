355b702637f53e3fdf693e7ef1a67c4d2d279273
Madeleine Clore <madeleine.clore@gmail.com>
2019-11-24 20:34:21 -0800

    AddCalculationsButton - add button to homescreen to link to calculations screen

--- CO2-U/screens/calculationsScreen.js
+++ CO2-U/screens/calculationsScreen.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
+import { ScrollView, StyleSheet } from 'react-native';
 
 export default function CalculationsScreen() {
-  /**
-   * Go ahead and delete ExpoConfigView and replace it with your content;
-   * we just wanted to give you a quick view of your config.
-   */
-  return <ExpoConfigView />;
+  return (
+    <ScrollView style={styles.container}>
+      {/**
+       * Go ahead and delete ExpoLinksView and replace it with your content;
+       * we just wanted to provide you with some helpful links.
+       */}
+    </ScrollView>
+  );
 }
 
-AchievementsScreen.navigationOptions = {
-  title: 'app.json',
-};
+// Calculations.navigationOptions = {
+//   title: 'Calculations',
+// };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: '#fff',
+  },
+});
