5bfaf1b5cc6205dbc7d3f19ef8ec7a4830250029
alanakai <56269477+alanakai@users.noreply.github.com>
2019-12-11 09:58:30 -0800

    changeActiveGoalsScreen - make active goals screen prettier for show and tell

--- CO2-U/screens/ActiveGoalsScreen.js
+++ CO2-U/screens/ActiveGoalsScreen.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet, View, Image, Text } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 15,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
   },
 });
 
@@ -18,12 +18,27 @@ export default class ActiveGoalsScreen extends React.Component {
 
   render() {
     return (
-      <ScrollView style={styles.container}>
+      <ScrollView style={styles.container}
+        contentContainerStyle={styles.contentContainer}>
+        <View style={styles.titleContainer}>  {/*this code is only temp stand-in (just looks good)*/}
+          <Text style={styles.titleText}>Goal 1</Text> 
+          {/*displays text on L edge of screen, in HomeScreen this same code displays text in the middle, idk why*/}
+          <View style={{ width: 500, height: 100, backgroundColor: 'powderblue' }} />
+          {/*displays box of color, will not be gaurenteed to fill whole screen on bigger phone, could just make huge num and never go away?*/}
+          <Text style={styles.titleText}>Goal 2</Text>
+          <View style={{ width: 500, height: 100, backgroundColor: 'skyblue' }} />
+          <Text style={styles.titleText}>Goal 3</Text>
+          <View style={{ width: 500, height: 100, backgroundColor: 'steelblue' }} />
+        </View>
+
         {/**
          * Go ahead and delete ExpoLinksView and replace it with your content;
          * we just wanted to provide you with some helpful links.
-         */}
-        <ExpoLinksView />
+      *<View style={{ flex: 20, flexDirection: 'col' }}> THIS CODE DISPLAYS COLORED BOXES IN A COLLUM, COULD REPLACE COL WITH ROW TOO
+        *<View style={{ width: 50, height: 50, backgroundColor: 'skyblue' }} />
+        *<View style={{ width: 50, height: 50, backgroundColor: 'steelblue' }} />
+      *</View>
+        */}
       </ScrollView>
     );
   }
