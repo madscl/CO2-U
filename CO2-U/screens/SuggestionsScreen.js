102d1950c9d994378ecd2fc9bedf35bfbb90060a
echurley5 <erica.c.hurley@gmail.com>
2019-12-17 09:17:07 -0800

    ImportingFirebase-firebase baby

--- CO2-U/screens/SuggestionsScreen.js
+++ CO2-U/screens/SuggestionsScreen.js
@@ -10,17 +10,57 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
+import * as firebase from 'firebase';
 
-export default function SuggestionsScreen() {
-  return (
-    <ScrollView style={styles.container}>
-      <Text style={styles.getStartedText}>Here's how to reduce your carbon footprint</Text>
-      <Text style={styles.getStartedText}></Text>
-      <Text style={styles.getAttentionText}>Daily Challenge:</Text> 
-      <Text style={styles.dailyChallengeText}>Try to eat 2 plant based meals today!</Text>
-    </ScrollView>
-  );
+
+//setting up firebase
+var firebaseConfig = {
+  apiKey: "AIzaSyBK6JW-daTxb4cdaU0lvopE3dWJUeeLI-Q",
+  authDomain: "coandu-b7fb5.firebaseapp.com",
+  databaseURL: "https://coandu-b7fb5.firebaseio.com",
+  projectId: "coandu-b7fb5",
+  storageBucket: "coandu-b7fb5.appspot.com",
+  messagingSenderId: "1090602721142",
+  appId: "1:1090602721142:web:5a33a3e8013df169fce457",
+  measurementId: "G-JL044NSZGD"
+};
+
+//initialize firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+//firebase.analytics();
+
+
+export default class SuggestionsScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    //updates daily challenge value
+    this.state = {
+      daily: "Loading..."
+    }
+    this.setupDailyChallengeListener()
+  }
+
+  setupDailyChallengeListener() {
+    //references daily challenge branch and snapshots value
+    firebase.database().ref('daily-challenge').on('value', (snapshot) => {
+      console.log(snapshot.val());
+      //sets state as value
+      this.setState({ daily: snapshot.val()})
+    });
+  }
+
+  render() {
+    return (
+      <ScrollView style={styles.container}>
+        <Text style={styles.getStartedText}>Here's how to reduce your carbon footprint</Text>
+        <Text style={styles.getStartedText}></Text>
+        <Text style={styles.getAttentionText}>Daily Challenge:</Text> 
+        <Text style={styles.getAttentionText}>{this.state.daily}</Text>
+        </ScrollView>
+    );
+  }
 }
+
 
 SuggestionsScreen.navigationOptions = {
   title: 'Suggestions',
